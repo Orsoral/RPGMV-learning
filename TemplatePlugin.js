d507b9772e95ec87680444e67191bdd7a63e1dd6
Orsoral <25392002+Orsoral@users.noreply.github.com>
2019-02-01 10:57:26 -0800

    Update TemplatePlugin.js
    added example for accessing plug-in parameters

--- TemplatePlugin.js
+++ TemplatePlugin.js
@@ -4,6 +4,7 @@
  * @desc	Description of the parameters.
  * @default	Default values of the parameters.
  * @help	A detailed description of the plugin.
+ * adds a manual break line
  */
 
 //Initialize the ability to use plug-in commands
@@ -21,6 +22,13 @@
 //Overwrite a function in the Game
 
 (function(){
+  // create a variable to store parameters - it will be an array
+  //load plugin parameters, name of the plugin file without the extension between the quotes, without .js extension
+  //var could be called more vague, such as parameters because its local. I prefer to be more specific by appending hte file name to the variable name
+  var parameters_TemplatePlugin= PluginManager.parameters('TemplatePlugin');
+  //To access a value of one of the parameters, identify it between quotes in square brackets. var is a string by default.
+  var shortNameOfParameter = parameters_TemplatePlugin['Short Name of the parameters.'];
+ 
   //to be compatible with other plug-ins that modify same functions
   //store its function in a variable first
   //variable name is structured: name_of_Class_nameOfFunction_previousBehavior
@@ -32,6 +40,6 @@
     //call the previous behavior you want to keep, including parameter listed when you inserted the function to override
     _Game_Player_executeMove_previousBehavior.call(this, direction)
     //add your custom behavior below
-    $gameMessage.add("hi");
+    $gameMessage.add('hi, ' + shortNameOfParameter);
   }
 })();
